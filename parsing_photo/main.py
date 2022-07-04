a774927073004cc4fd507cf2216447d5784e5a06
zhiidetopchubaeva <ms.jiyd@gmail.com>
2022-07-04 12:30:30 +0600

    oop-shop

---
+++ parsing_photo/main.py
@@ -0,0 +1,13 @@
+import requests
+url = 'https://fs.tonkosti.ru/12/3x/123xdouxq300o0gww0cs0kgow.jpg'
+
+res= requests.get(url)
+
+name = "photos/photo1.jpg"
+with open(name, "wb") as file:
+    file.write(res.content)
+# print(res) # <Response [200]>
+# print(dir(res))
+# # ['__attrs__', '__bool__', '__class__', '__delattr__', '__dict__', '__dir__', '__doc__', '__enter__', '__eq__', '__exit__', '__format__', '__ge__', '__getattribute__', '__getstate__', '__gt__', '__hash__', '__init__', '__init_subclass__', '__iter__', '__le__', '__lt__', '__module__', '__ne__', '__new__', '__nonzero__', '__reduce__', '__reduce_ex__', '__repr__', '__setattr__', '__setstate__', '__sizeof__', '__str__', '__subclasshook__', '__weakref__', '_content', '_content_consumed', '_next', 'apparent_encoding', 'close', 'connection', 'content', 'cookies', 'elapsed', 'encoding', 'headers', 'history', 'is_permanent_redirect', 'is_redirect', 'iter_content', 'iter_lines', 'json', 'links', 'next', 'ok', 'raise_for_status', 'raw', 'reason', 'request', 'status_code', 'text', 'url']
+# print(res.status_code) # 200
+
