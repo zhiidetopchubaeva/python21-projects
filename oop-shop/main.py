8a1d15de9ee87481533f293e16ccd0194c470144
zhiidetopchubaeva <ms.jiyd@gmail.com>
2022-07-06 12:56:28 +0600

    shop update

--- oop-shop/main.py
+++ oop-shop/main.py
@@ -1,12 +1,18 @@
-from nis import cat
+
 from shop.models import Product, Category
-from shop.serializers import ProductSerializer
+from shop.views import product_create, product_detail, product_list, product_update
 
 cat = Category("phones")
+Category("dyson")
+Category("food")
+
 obj1 = Product("iphone 10", 234, "...", 3, cat)
 obj2 = Product("iphone 11", 200, "...", 10, cat)
 obj3 = Product("iphone 13", 239, "...", 5, cat)
 
-res = ProductSerializer().serialize_queryset([obj1, obj2, obj3])
+
 from pprint import pprint # для вывода в строку
-pprint(res)
+pprint(product_create())
+pprint(product_list())
+id_ = input("Введите продукт для обновления: ")
+pprint(product_update(id_))
